9a6b2e3cad61a11aa1adbc42064d63d2d2cfea8c
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 08:40:44 +0700

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes} from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
 
 
-
+const routes: Routes = [
+  {path: '', component: HomepageComponent}
+];
 @NgModule({
   declarations: [],
   imports: [
