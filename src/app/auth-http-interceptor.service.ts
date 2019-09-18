dc4c47e02b2a1e21cb761ade17dcaa751f67ea8f
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 22:47:28 +0700

    import library

--- src/app/auth-http-interceptor.service.ts
+++ src/app/auth-http-interceptor.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
