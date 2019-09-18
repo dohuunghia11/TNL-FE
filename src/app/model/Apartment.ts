61dda2d008abd92cd956bbc76a6fa3f47d52ef83
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 08:49:35 +0700

    add Apartment Model

---
+++ src/app/model/Apartment.ts
@@ -0,0 +1,10 @@
+export interface Apartment {
+  id: number;
+  name: string;
+  address: string;
+  numberOfRooms: string;
+  numberOfBathrooms: string;
+  description: string;
+  prize: string;
+  area: string;
+}
