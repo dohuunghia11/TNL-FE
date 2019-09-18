21772e685720b0cd5ca663f11f9b7183b6c64e53
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 10:30:09 +0700

    add more apartment test data

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -20,8 +20,31 @@ export class HomepageComponent implements OnInit {
         'hồng. Landmark81 rất gần, bạn sẽ cảm thấy như bạn có thể vươn ra và chạm vào nó. Bạn sẽ thực sự được tận hưởng một Sài Gòn' +
         ' của riêng bạn. Căn hộ được xây dựng với những tiện nghi tốt nhất trong khu vực. Với nội thất hoàn toàn mới, nơi bạn có thể ' +
         'kiểm soát khí hậu trong nhà của riêng bạn. Nhà bếp với các thiết bị tốt nhất và 3 phòng ngủ có kích thước lớn với khăn trải' +
-        ' giường tốt nhất và đồ nội thất sang trọng để chiều chuộng bạn.', area: '2000 m2',
+        ' giường tốt nhất và đồ nội thất sang trọng để chiều chuộng bạn.', area: '2000 m2', category: 'apartment',
       image: 'https://cdn.luxstay.com/rooms/19033/large/room_19033_97_1547110185.jpg', prize: '2500.000', rate: '3'
+    },
+    {
+      id: 2, name: 'Homelead_Flamingo', address: 'Phuc Yen, Vinh Phuc, VietNam', numberOfRooms: '4', numberOfBathrooms: '3',
+      // tslint:disable-next-line:max-line-length
+      description: 'Villa Flamingo 02 phòng ngủ được thiết kế theo phong cách kiến trúc đương đại với các nét chấm phá tinh tế và góc cạnh. ' +
+        'Mỗi góc nhìn tại đây đều mở ra không gian ngoài trời với cảnh quan tuyệt đẹp phía trước và khuôn viên vườn xanh mát',
+      category: 'villa', image: 'https://cdn.luxstay.com/rooms/24653/large/room_24653_38_1563794487.jpg', area: '3000 m2',
+      prize: '3600000', rate: '4'
+    },
+    {
+      id: 3, name: 'Tom\'s Valley', address: 'Da Lat, Lam Dong, VietNam', numberOfRooms: '1', numberOfBathrooms: '2',
+      description: 'Căn hộ studio cực rộng áp mình bên sườn thung lũng, với 2 mặt kính nhìn ra không gian ngút ngàn phía dưới, ' +
+        'thực sự là một trải nghiệm tuyệt vời',
+      category: 'villa', image: 'https://cdn.luxstay.com/rooms/11978/large/1539665147_IMG-8631.JPG', area: '4000 m2',
+      prize: '1000000', rate: '3.5'
+    },
+    {
+      id: 4, name: 'LUXURY APARTMENT', address: 'Lieu Giai, Ba Dinh, Ha Noi, VietNam', numberOfRooms: '2', numberOfBathrooms: '2',
+      description: 'Căn hộ của chúng tôi ở tầng cao, view hồ, được trang bị đầy đủ nội thất sang trọng, diện tích 82m2, 2 phòng ngủ, ' +
+        '2 phòng tắm, 1 không gian sinh hoạt rộng rãi và bếp tách biệt. Bacony với ánh nắng mặt trời và tầm nhìn tuyệt vời, an ninh 24/7 ' +
+        'và đường dây nóng 24/7.',
+      category: 'apartment', image: 'https://cdn.luxstay.com/rooms/23763/large/room_23763_30_1555683880.jpg', area: '5000 m2',
+      prize: '1550000', rate: '4'
     }
   ];
   constructor(private apartmentService: ApartmentService) { }
