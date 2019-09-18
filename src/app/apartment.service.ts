ce68be06db5c8524c9742ef20765236b2b7b31e3
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 11:36:01 +0700

    edit apartment service

--- src/app/apartment.service.ts
+++ src/app/apartment.service.ts
@@ -62,16 +62,16 @@ export class ApartmentService {
   getApartments(): Observable<Apartment[]> {
     return this.http.get<Apartment[]>(this.API_URL);
   }
-  getBookById(id: number): Observable<Apartment> {
+  getApartmentById(id: number): Observable<Apartment> {
     return this.http.get<Apartment>(`${this.API_URL}/${id}`);
   }
-  createBook(book: Partial<Apartment>): Observable<Apartment> {
+  createApartment(book: Partial<Apartment>): Observable<Apartment> {
     return this.http.post<Apartment>(this.API_URL, book);
   }
-  updateBook(book: Apartment): Observable<Apartment> {
+  updateApartment(book: Apartment): Observable<Apartment> {
     return this.http.put<Apartment>(`${this.API_URL}/${book.id}`, book);
   }
-  deleteBook(id: number): Observable<Apartment> {
+  deleteApartment(id: number): Observable<Apartment> {
     return this.http.delete<Apartment>(`${this.API_URL}/${id}`);
   }
   constructor(private http: HttpClient) { }
