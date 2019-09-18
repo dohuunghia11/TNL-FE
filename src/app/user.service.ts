ab12d06408923069a2471cf202578eb66f898c18
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 16:12:43 +0700

    edit user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -8,16 +8,23 @@ import {User} from './model/User';
 })
 export class UserService {
 
-  private readonly API_URL = 'http://localhost:8080/api/apartments';
+  private readonly API_URL = 'http://localhost:8080/api/user';
 
   constructor(private httpClient: HttpClient) {
   }
 
-  create(user: Partial<User>): Observable<User> {
+  register(user: Partial<User>): Observable<User> {
     return this.httpClient.post<User>(this.API_URL, user);
   }
 
   update(user: User): Observable<User> {
     return this.httpClient.put<User>(`${this.API_URL}/${user.id}`, user);
+  }
+
+  getById(id: number): Observable<User> {
+    return this.httpClient.get<User>(`${this.API_URL}/${id}`);
+  }
+  delete(id: number): Observable<User> {
+    return this.httpClient.delete<User>(`${this.API_URL}/${id}`);
   }
 }
