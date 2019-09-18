01966866a376933635902161512ca0a5d6a4946c
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 22:51:56 +0700

    build login function

--- src/app/apartment.service.ts
+++ src/app/apartment.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import {Apartment} from './model/Apartment';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AuthenticationService} from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApartmentService {
   apartment: Apartment[];
+  header: Headers;
   private readonly API_URL = 'http://localhost:8080/api/apartments';
 
   getApartments(): Observable<Apartment[]> {
-    return this.http.get<Apartment[]>(this.API_URL);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.authenService.token}`,
+      })
+    };
+    console.log(httpOptions);
+    return this.http.get<Apartment[]>(this.API_URL, httpOptions);
   }
   getApartmentById(id: number): Observable<Apartment> {
     return this.http.get<Apartment>(`${this.API_URL}/${id}`);
@@ -25,5 +34,7 @@ export class ApartmentService {
   deleteApartment(id: number): Observable<Apartment> {
     return this.http.delete<Apartment>(`${this.API_URL}/${id}`);
   }
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authenService: AuthenticationService) {
+    console.log(this.header);
+  }
 }
