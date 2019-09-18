e0cbc69a2ed4aabc99e721d877760031d1621722
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-18 11:29:41 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {AppRoutingModule} from './app-routing.module';
 import { SearchComponent } from './search/search.component';
 import {ApartmentDetailComponent} from './apartment-detail/apartment-detail.component';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {FormsModule} from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
