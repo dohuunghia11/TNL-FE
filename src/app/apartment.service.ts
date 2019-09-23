cdd64a265d4391d7d9d386099a2ffd3e38ae9518
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-23 10:05:51 +0700

    add create-apartment button edit url

--- src/app/apartment.service.ts
+++ src/app/apartment.service.ts
@@ -9,29 +9,69 @@ import {environment} from '../environments/environment';
   providedIn: 'root'
 })
 export class ApartmentService {
+  // apartment: Apartment[];
+  // private API_URL = environment.URL + '/api/apartments';
+  //
+  // constructor(private http: HttpClient, private authenService: AuthenticationService) {
+  // }
+  //
+  // getApartments(): Observable<any> {
+  //   return this.http.get<any>(this.API_URL);
+  // }
+  //
+  // getApartmentById(id: number): Observable<any> {
+  //   return this.http.get<any>(`${this.API_URL}/${id}`);
+  // }
+  //
+  // createApartment(apartment: Partial<Apartment>): Observable<Apartment> {
+  //   return this.http.post<Apartment>(this.API_URL, apartment);
+  // }
+  //
+  // updateApartment(apartment: Apartment): Observable<Apartment> {
+  //   return this.http.put<Apartment>(`${this.API_URL}/${apartment.id}`, apartment);
+  // }
+  //
+  // deleteApartment(id: number): Observable<Apartment> {
+  //   return this.http.delete<Apartment>(`${this.API_URL}/${id}`);
+  // }
   apartment: Apartment[];
-  private API_URL = environment.URL + '/api/apartments';
+  private readonly API_URL = 'http://localhost:8080/api/apartments';
 
-  constructor(private http: HttpClient, private authenService: AuthenticationService) {
+  getApartments(): Observable<Apartment[]> {
+    return this.http.get<Apartment[]>(this.API_URL, {
+        headers: this.authenService.header
+      }
+    );
   }
 
-  getApartments(): Observable<any> {
-    return this.http.get<any>(this.API_URL);
-  }
-
-  getApartmentById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.API_URL}/${id}`);
+  getApartmentById(id: number): Observable<Apartment> {
+    return this.http.get<Apartment>(`${this.API_URL}/${id}`,
+      {
+        headers: this.authenService.header
+      });
   }
 
   createApartment(apartment: Partial<Apartment>): Observable<Apartment> {
-    return this.http.post<Apartment>(this.API_URL, apartment);
+    return this.http.post<Apartment>(this.API_URL, apartment,
+      {
+        headers: this.authenService.header
+      });
   }
 
   updateApartment(apartment: Apartment): Observable<Apartment> {
-    return this.http.put<Apartment>(`${this.API_URL}/${apartment.id}`, apartment);
+    return this.http.put<Apartment>(`${this.API_URL}/${apartment.id}`, apartment,
+      {
+        headers: this.authenService.header
+      });
   }
 
   deleteApartment(id: number): Observable<Apartment> {
-    return this.http.delete<Apartment>(`${this.API_URL}/${id}`);
+    return this.http.delete<Apartment>(`${this.API_URL}/${id}`,
+      {
+        headers: this.authenService.header
+      });
+  }
+
+  constructor(private http: HttpClient, private authenService: AuthenticationService) {
   }
 }
