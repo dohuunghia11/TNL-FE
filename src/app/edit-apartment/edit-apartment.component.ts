30adeb9a35ae52bd2490c5b57f8c461984f3e24c
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-25 23:01:21 +0700

    add edit-apartment , profile-user component

---
+++ src/app/edit-apartment/edit-apartment.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Apartment} from '../model/Apartment';
+import {ApartmentService} from '../apartment.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-edit-apartment',
+  templateUrl: './edit-apartment.component.html',
+  styleUrls: ['./edit-apartment.component.css']
+})
+export class EditApartmentComponent implements OnInit {
+
+  apartmentForm: FormGroup;
+  apartment: Apartment;
+
+  constructor(private apartmentService: ApartmentService,
+              private route: ActivatedRoute,
+              private router: Router) {
+    this.apartmentForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      category: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      numberOfRooms: new FormControl(''),
+      numberOfBathrooms: new FormControl(''),
+      price: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      images: new FormControl('', [Validators.required]),
+      area: new FormControl('', [Validators.required]),
+      startDate: new FormControl(),
+      endDate: new FormControl()
+    });
+    this.apartment = {
+      id: 0,
+      name: '',
+      category: '',
+      address: '',
+      numberOfRooms: '',
+      numberOfBathrooms: '',
+      price: '',
+      description: '',
+      images: [],
+      rate: '',
+      area: '',
+      startDate: null,
+      endDate: null,
+      status: '',
+      user: ''
+    };
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      const id = +paramMap;
+      this.apartmentService.getApartmentById(id).subscribe(next => {
+        this.apartment = next.data;
+        console.log(next.data);
+      }, error1 => {
+        console.log(error1);
+        this.apartment = null;
+      });
+    });
+  }
+
+  editApartment() {
+    console.log(this.apartment);
+    this.apartmentService.updateApartment(this.apartment).subscribe(next => {
+      alert(next.message);
+    }, error => console.log(error));
+  }
+}
